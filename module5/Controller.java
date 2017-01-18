08c01eaa7acf87674f8cef761c13d9e9b8f674fb
javase <java.se@mail.ru>
2017-01-18 16:01:29 +0200

    add HomeWork 5.6 not end

--- module5/Controller.java
+++ module5/Controller.java
@@ -1,17 +1,44 @@
 package module5;
 
-/**
- * Created by pan on 10.01.17.
- */
-public class Controller implements API{
+public class Controller {
     private API apis[] = new API[3];
 
-    @Override
-    public Room[] findRooms(int price, int preson, String city, String hotel) {
-        return new Room[0];
+    public Controller() {
+        BookingComAPI bookingComAPI = new BookingComAPI();
+        GoogleAPI googleAPI = new GoogleAPI();
+        TripAdvisorAPI tripAdvisorAPI = new TripAdvisorAPI();
+
+        apis[0] = bookingComAPI;
+        apis[1] = googleAPI;
+        apis[2] = tripAdvisorAPI;
+
     }
 
     Room[] requstRooms(int price, int persons, String city, String hotel) {
-        return new Room[0];
+        Room[] result = new Room[15];
+        int roomCount = 0;
+        BookingComAPI bookingComAPI = new BookingComAPI();
+        GoogleAPI googleAPI = new GoogleAPI();
+        TripAdvisorAPI tripAdvisorAPI = new TripAdvisorAPI();
+
+
+        if (bookingComAPI.findRooms(price, persons, city, hotel) != null)
+            result = bookingComAPI.findRooms(price, persons, city, hotel);
+        if (googleAPI.findRooms(price, persons, city, hotel) != null)
+            result = googleAPI.findRooms(price, persons, city, hotel);
+        if (tripAdvisorAPI.findRooms(price, persons, city, hotel) != null) {
+            result = tripAdvisorAPI.findRooms(price, persons, city, hotel);
+        }
+        
+
+        return result;
     }
+
+    Room[] check(API api1, API api2) {
+        Room[] res1 = api1.findRooms(0, 0, null, null);
+        Room[] res2 = api2.findRooms(0, 0, null, null);
+        return res1;
+    }
+
+
 }
